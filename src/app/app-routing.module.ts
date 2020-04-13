ddb84fe17299a5cc08db4be9f6f11bc17467441b
Tore Sveaass <tore.sveaass@gmail.com>
2020-04-13 17:55:46 +0200

    lagt til routing og laget skygger

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MinSideComponent } from './min-side/min-side.component';
+import { DagensSkjemaComponent } from './dagens-skjema/dagens-skjema.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dagens-skjema', component: DagensSkjemaComponent},
+  { path: 'min-side', component: MinSideComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
